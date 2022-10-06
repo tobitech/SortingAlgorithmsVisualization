870336b22554ddc0d9f102abaac3eca2a33e4323
Oluwatobi Omotayo <tobitech@ymail.com>
2022-10-06 11:22:48 +0100

    project setup.
    add a bar chart to the content view and a bubble sort algorithm to sort the data.
    
    add a delay with task.sleep for each swap to aid visualisation.

--- SortingAlgorithmsVisualization/ContentView.swift
+++ SortingAlgorithmsVisualization/ContentView.swift
@@ -1,16 +1,57 @@
-//
-//  ContentView.swift
-//  SortingAlgorithmsVisualization
-//
-//  Created by Oluwatobi Omotayo on 06/10/2022.
-//
-
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    
+    var input: [Int] {
+        var array = [Int]()
+        for i in 1...30 {
+            array.append(i)
+        }
+        
+        return array.shuffled()
+    }
+    
+    @State var data = [Int]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Chart {
+                ForEach(
+                    Array(zip(data.indices, data)), id: \.0
+                ) { index, item in
+                    BarMark(
+                        x: .value("Position", index),
+                        y: .value("Value", item))
+                }
+            }
+            .frame(width: 280, height: 250)
+            
+            Button("Sort it!") {
+                Task {
+                    try await bubbleSort()
+                }
+            }
+        }
+        .onAppear {
+            data = input
+        }
+    }
+    
+    func bubbleSort() async throws {
+        // first check if it's just one element otherwise it's already sorted.
+        guard data.count > 1 else { return }
+        
+        // now iterate over the array
+        for i in 0..<data.count {
+            for j in 0..<data.count - i - 1 {
+                if data[j] > data[j + 1] {
+                    data.swapAt(j + 1, j)
+                    // add a sleep just for visualisation purposes
+                    try await Task.sleep(until: .now.advanced(by: .milliseconds(500)), clock: .continuous)
+                }
+            }
+        }
     }
 }
 
