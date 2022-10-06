53bc31106c83e34f873baf0da4ba68d18363ceeb
Oluwatobi Omotayo <tobitech@ymail.com>
2022-10-06 10:40:08 +0100

    Initial Commit

---
+++ SortingAlgorithmsVisualization/SortingAlgorithmsVisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingAlgorithmsVisualizationApp.swift
+//  SortingAlgorithmsVisualization
+//
+//  Created by Oluwatobi Omotayo on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SortingAlgorithmsVisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
